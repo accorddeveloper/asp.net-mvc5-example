2d1b0e337288f070007663224b82355a4812c07d
suonto <markus.suonto@eficode.com>
2017-04-04 05:37:24 -0700

    Customers and Movies

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ViewResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Hancock" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+
+            var viewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Hancock" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+
+            foreach (var customer in customers)
+            {
+                if (customer.Id == id)
+                {
+                    return View(customer);
+                }
+            }
+            return new HttpNotFoundResult();
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,41 +10,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ViewResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() { Name = "Shrek" };
-            var customers = new List<Customer>
+            var movies = new List<Movie>
             {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
+                new Movie { Name = "Shrek" },
+                new Movie { Name = "Beauty and the beast" }
             };
 
-            var viewModel = new RandomMovieViewModel
+            var viewModel = new MoviesViewModel
             {
-                Movie = movie,
-                Customers = customers
+                Movies = movies
             };
-
             return View(viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
